a5afcf36d230a770dda3f4161ccffa9b9d11835c
alejandronegri <alejandronegri@outlook.com>
2025-04-03 12:17:57 -0300

    lint fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { Button } from "@/components/ui/button"
-import { Code, ExternalLink, Github } from "lucide-react"
+import { Code } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
 import { experience } from "./data/experience"
